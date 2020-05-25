703c6e1d136123eed457d24184fab2ff0fc6bd00
Sandip Patil <sanppatil@gmail.com>
2020-05-25 01:33:31 -0400

    initial commit

--- commands.sh
+++ commands.sh
@@ -1,7 +1,8 @@
 
 # Start producer to publish messages
- kafka-console-producer --broker-list localhost:9092 --topic streams-input-topic --property parse.key=true --property key.separator=:
+ kafka-console-producer --broker-list localhost:9092 --topic stateless-transformations-input-topic --property parse.key=true --property key.separator=:
 
 # Start consumer to receive messages
- kafka-console-consumer --bootstrap-server localhost:9092 --topic streams-output-topic --property print.key=true
+ kafka-console-consumer --bootstrap-server localhost:9092 --topic stateless-transformations-output-topic --property print.key=true
+
  
--- src/main/java/com/kafka/stream/StatelessTransformationsMain.java
+++ src/main/java/com/kafka/stream/StatelessTransformationsMain.java
@@ -1,21 +1,24 @@
 package com.kafka.stream;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 
-public class StreamsMain {
+public class StatelessTransformationsMain {
 
 	public static void main(String[] args) {
 		// Set up the configuration.
 		final Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stateless-transformations-example");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 		// Since the input topic uses Strings for both key and value, set the default
@@ -25,9 +28,38 @@ public class StreamsMain {
 
 		// Get the source stream.
 		final StreamsBuilder builder = new StreamsBuilder();
-		final KStream<String, String> source = builder.stream("streams-input-topic");
+		final KStream<String, String> source = builder.stream("stateless-transformations-input-topic");
 
-		source.to("streams-output-topic");
+		// Split the stream into two streams, one containing all records where the key
+		// begins with "a", and the other containing all other records.
+		KStream<String, String>[] branches = source.branch((key, value) -> key.startsWith("a"), (key, value) -> true);
+		KStream<String, String> aKeysStream = branches[0];
+		KStream<String, String> othersStream = branches[1];
+
+		// Remove any records from the "a" stream where the value does not also start
+		// with "a".
+		aKeysStream = aKeysStream.filter((key, value) -> value.startsWith("a"));
+
+		// For the "a" stream, convert each record into two records, one with an
+		// uppercased value and one with a lowercased value.
+		aKeysStream = aKeysStream.flatMap((key, value) -> {
+			List<KeyValue<String, String>> result = new LinkedList<>();
+			result.add(KeyValue.pair(key, value.toUpperCase()));
+			result.add(KeyValue.pair(key, value.toLowerCase()));
+			return result;
+		});
+
+		// For the "a" stream, modify all records by uppercasing the key.
+		aKeysStream = aKeysStream.map((key, value) -> KeyValue.pair(key.toUpperCase(), value));
+
+		// Merge the two streams back together.
+		KStream<String, String> mergedStream = aKeysStream.merge(othersStream);
+
+		// Print each record to the console.
+		mergedStream.peek((key, value) -> System.out.println("key=" + key + ", value=" + value));
+
+		// Output the transformed data to a topic.
+		mergedStream.to("stateless-transformations-output-topic");
 
 		final Topology topology = builder.build();
 		final KafkaStreams streams = new KafkaStreams(topology, props);
@@ -37,7 +69,7 @@ public class StreamsMain {
 
 		// Attach a shutdown handler to catch control-c and terminate the application
 		// gracefully.
-		Runtime.getRuntime().addShutdownHook(new Thread("shutdown-hook") {
+		Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
 			@Override
 			public void run() {
 				streams.close();
